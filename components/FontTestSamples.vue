b29b35002d6077b09e0333f3837461ec75233f89
Federico Biondi <fdbion@gmail.com>
2021-11-30 21:21:43 -0300

    fonts samples

---
+++ components/FontTestSamples.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="text-white flex items-center mt-12 overflow-hidden">
+    <Logo class="text-center w-1/2"/>
+
+    <div class="w-1/2">
+      <p class="font-muli">
+        Scale fast your business with expert software engineering teams.
+      </p>
+      <p class="font-sohne-var">
+        Scale fast your business with expert software engineering teams.
+      </p>
+      <p class="font-source-code-pro">
+        Scale fast your business with expert software engineering teams.
+      </p>
+      <p class="font-roboto-mono">
+        Scale fast your business with expert software engineering teams.
+      </p>
+
+      <div class="font-muli font-extrabold" data-aos="slide-left">
+        <h2>Pollux</h2>
+      </div>
+
+      <div class="font-sohne-var" data-aos="zoom-out-left">
+        <h2>Pollux</h2>
+      </div>
+
+      <div class="font-source-code-pro" data-aos="flip-left">
+        <h2>Pollux</h2>
+      </div>
+
+      <div class="font-roboto-mono" data-aos="fade-down-left">
+        <h2>Pollux</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 132px;
+}
+
+p {
+  font-size: 28px;
+}
+
+.font-source-code, .font-roboto-mono {
+  font-weight: 400;
+}
+</style>
