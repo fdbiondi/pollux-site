847f61f6d744462645dc99c5cde5da16d5eaf6d7
Federico Biondi <fdbion@gmail.com>
2021-11-30 21:20:12 -0300

    create logo

---
+++ components/Logo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="font-fredoka-one text-white">
+      <div class="px-1 tracking-wider text-7xl">Pollux</div>
+      <div class="text-rose-500 text-2xl">Software Engineering</div>
+    </div>
+    <div class="overlay"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+  width: auto;
+  height: auto;
+  margin: auto;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  cursor: pointer;
+}
+</style>
