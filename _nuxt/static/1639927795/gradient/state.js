b4490a0352bbce1c15321cd0977adfc45704332f
fdbiondi <fdbiondi@users.noreply.github.com>
2021-12-19 15:30:22 +0000

    deploy: 824bd2afa19b58fe45ebfb93c68ac71002934152

--- _nuxt/static/1639927795/gradient/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpollux-site\u002F_nuxt\u002Fstatic\u002F1639927795",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgradient",config:{_app:{basePath:"\u002Fpollux-site\u002F",assetsPath:"\u002Fpollux-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));
