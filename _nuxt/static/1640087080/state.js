130dd66be0996c7c88d07c00ae2d4cf13426c9a5
fdbiondi <fdbiondi@users.noreply.github.com>
2021-12-23 18:52:14 +0000

    deploy: 98c0a10486d01cc4acd01abb9fbb18b670315a99

--- _nuxt/static/1640087080/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpollux-site\u002F_nuxt\u002Fstatic\u002F1640087080",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpollux-site\u002F",assetsPath:"\u002Fpollux-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));
