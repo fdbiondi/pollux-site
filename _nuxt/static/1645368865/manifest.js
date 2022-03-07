a621c8a74e34cfcce34aa15f47c3fa34c4cf85fc
fdbiondi <fdbiondi@users.noreply.github.com>
2022-03-07 00:00:03 +0000

    deploy: b54e637bc6d12370a9ee4ac67ca820a3801abb4d

--- _nuxt/static/1645368865/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ffont-samples","\u002Fgradient"]})
---
+++ _nuxt/static/1646611201/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002F404","\u002Ffont-samples","\u002Fgradient","\u002Fprivacy-policy"]})
