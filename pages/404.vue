1e702d76cbeecfce2fc1666013cf766892bfb9ab
Federico Biondi <fdbion@gmail.com>
2022-03-19 20:16:32 -0300

    fix: border color

--- pages/404.vue
+++ pages/404.vue
@@ -34,7 +34,7 @@
     </div>
 
     <div
-      class="container mx-auto border-t border-gray-800 pt-4 text-center font-mono uppercase"
+      class="container mx-auto border-t border-gray-800 pt-4 text-center font-mono uppercase dark:border-gray-200"
     >
       <div class="mb-1 flex justify-center gap-3">
         <nuxt-link to="#" class="hover:underline">
