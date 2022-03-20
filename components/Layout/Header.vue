26243803852732be1849e37edb37bb0536ace884
Federico Biondi <fdbion@gmail.com>
2022-03-20 10:07:21 -0300

    feat: menu button

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -6,7 +6,7 @@
       </a>
 
       <div class="z-20 flex items-center">
-        <div class="hidden text-lg text-white xl:flex">
+        <div class="hidden text-lg text-white 2xl:flex">
           <a href="#services" class="nav-item route--underline"> Services </a>
           <a href="#our-clients" class="nav-item route--underline">
             Our Clients
@@ -18,8 +18,21 @@
           <ThemeSwitch />
         </div>
 
-        <div class="block xl:hidden">
-          <button
+        <div class="block 2xl:hidden">
+          <div class="header__hamburger header__item h-align-left">
+            <a
+              href="#header"
+              aria-label="Hamburger Menu"
+              class="hamburger"
+              :class="{ 'is-active': menuOpen }"
+              @click="menuOpen = !menuOpen"
+            >
+              <div class="hamburger__slice"></div>
+              <div class="hamburger__slice"></div>
+            </a>
+          </div>
+
+          <!-- <button
             class="flex items-center rounded border border-gray-400 px-4 py-3 text-gray-200 focus:outline-none"
           >
             <svg
@@ -31,6 +44,7 @@
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
           </button>
+           -->
         </div>
       </div>
     </nav>
@@ -48,11 +62,88 @@ export default {
     Logo,
     ThemeSwitch,
   },
+
+  data() {
+    return {
+      menuOpen: false,
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .nav-item {
   @apply mr-12 block filter drop-shadow-tight lg:inline-block;
+}
+
+.hamburger {
+  &__slice {
+  }
+}
+
+.header__item {
+  float: left;
+  clear: none;
+  text-align: inherit;
+  width: 33.33333333333333%;
+  margin-left: 0;
+  margin-right: 0;
+}
+
+.header__hamburger {
+  display: inline-block;
+  padding-left: 0.875rem;
+}
+
+.header__hamburger .hamburger {
+  display: block;
+  width: 2rem;
+  margin-right: 1.25rem;
+  cursor: pointer;
+}
+
+.header__hamburger .hamburger__slice {
+  display: block;
+  height: 0.125rem;
+  margin: 0.5rem 0;
+  background-color: hsla(0, 0%, 100%, 0.6);
+  transition: all 0.15s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+}
+
+.header__hamburger .hamburger:hover .hamburger__slice {
+  background-color: #dac19d;
+  transition: all 0.15s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+}
+
+.header__hamburger .hamburger__slice:first-of-type {
+  width: 2rem;
+}
+
+.header__hamburger .hamburger__slice:last-of-type {
+  width: 1.375rem;
+}
+
+.header__hamburger .hamburger:hover .hamburger__slice:first-of-type {
+  width: 1.375rem;
+}
+
+.header__hamburger .hamburger:hover .hamburger__slice:last-of-type {
+  width: 2rem;
+}
+
+.header__hamburger .hamburger.is-active .hamburger__slice {
+  width: 2rem !important;
+  background-color: #fff;
+  -webkit-animation: none !important;
+  animation: none !important;
+  transform-origin: center;
+}
+
+.header__hamburger .hamburger.is-active .hamburger__slice:first-of-type {
+  transform: rotate(45deg) translate(0.125rem, 0.3125rem);
+}
+
+.header__hamburger .hamburger.is-active .hamburger__slice:last-of-type {
+  transform: rotate(-45deg) translate(0.125rem, -0.3125rem);
 }
 </style>
