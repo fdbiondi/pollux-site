80ac0158b6377f56af6d983e19ecfa8c17b4edc5
fbiondi <federico.biondi@rms.com>
2023-05-21 20:24:14 -0300

    wip

--- components/Common/UI/TiltDivider.vue
+++ components/Common/UI/TiltDivider.vue
@@ -6,7 +6,10 @@
       viewBox="0 0 1200 120"
       preserveAspectRatio="none"
     >
-      <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" :class="tiltColor"></path>
+      <path
+        d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+        :class="tiltColor"
+      ></path>
     </svg>
   </div>
 </template>
