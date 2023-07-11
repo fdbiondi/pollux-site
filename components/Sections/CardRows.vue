fec4b9c547d4fe4d27e51c7ba73ce320fcaed1a6
julian <julian@polluxcoop.com>
2023-07-11 18:11:24 -0300

    wip: change column cards for simple cards

---
+++ components/Sections/CardRows.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container mx-auto">
+    <ul
+      role="list"
+      class="grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3"
+    >
+      <li
+        v-for="item in items"
+        :key="item.title"
+        class="col-span-1 flex rounded-md shadow-sm"
+      >
+        <div
+          class="flex w-5 flex-shrink-0 rounded-l-md"
+          :class="item.bgColor"
+        ></div>
+        <div
+          class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white"
+        >
+          <div class="flex-1 truncate px-4 py-2 text-sm">
+            <p class="font-medium text-gray-900">
+              {{ item.title }}
+            </p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    development: {
+      type: Array,
+      default: null,
+    },
+
+    design: {
+      type: Array,
+      default: null,
+    },
+  },
+
+  computed: {
+    items() {
+      return this.development.concat(this.design)
+    },
+  },
+}
+</script>
--- support/constants/home.js
+++ support/constants/home.js
@@ -3,24 +3,28 @@ export const design = [
     description: 'Take your business prototype app for a test drive',
     image: require('~/assets/images/icons/icons8-web-design-64.png'),
     title: 'UX Design & Testing',
+    bgColor: 'bg-pink-600',
   },
   {
     description:
       'Create a beautiful and understandable interface to delight your users',
     image: require('~/assets/images/icons/icons8-light-64.png'),
     title: 'UI Design & Branding',
+    bgColor: 'bg-gray-600',
   },
   {
     description:
       'Well-designed and colorful apps, personalized on your brand’s concept and values, to build trust with your users',
     image: require('~/assets/images/icons/icons8-tune-64.png'),
     title: 'Custom Designed',
+    bgColor: 'bg-green-600',
   },
   {
     description:
       'Responsive and accessible user interfaces that allow more people to access it from different types of devices with different needs',
     image: require('~/assets/images/icons/icons8-amiable-64.png'),
     title: 'User Friendly',
+    bgColor: 'bg-red-600',
   },
 ]
 
@@ -30,6 +34,7 @@ export const development = [
       'We have rock-solid DevOps/SLA practices to help you leverage cloud technology speeding up development and increasing app security and scalability.',
     image: require('~/assets/images/icons/icons8-cloud-done-64.png'),
     title: 'Cloud Technology',
+    bgColor: 'bg-blue-600',
   },
 
   {
@@ -37,6 +42,7 @@ export const development = [
       'Ship your MVP to market without spending months on assembling an in-house development team for your startup.',
     image: require('~/assets/images/icons/icons8-process-64.png'),
     title: 'MVP Development',
+    bgColor: 'bg-orange-600',
   },
 ]
 
