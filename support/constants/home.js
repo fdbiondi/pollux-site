18b031146f9b5515261064e117df3260b33ccb19
fbiondi <federico.biondi@rms.com>
2023-07-17 11:09:20 -0300

    fix: text and card spacing

--- support/constants/home.js
+++ support/constants/home.js
@@ -41,16 +41,6 @@ export const SERVICES_SECTION_TITLE = 'Find your software development Service'
 export const servicesTypes = [
   {
     description:
-      'Ship your MVP to market without spending months on assembling an in-house development team for your startup.',
-    title: 'MVP Development',
-  },
-]
-
-export const servicesSectionTitle = 'What we do'
-
-export const services = [
-  {
-    description:
       'From responsive design to backend development and deployment, we deliver high-quality web applications that enhance user experiences and drive your online presence forward.',
 
     image: './icons/icons8-source-code-64.png',
@@ -59,21 +49,24 @@ export const services = [
 
   {
     description:
-      'We specialize in Flutter mobile application development, providing robust solutions tailored to your needs. From UI/UX design to seamless integration and maintenance, we deliver innovative and high-performance mobile apps that engage users and drive business growth.',
-
+      'From UI/UX design to seamless integration and maintenance, we deliver innovative and high-performance mobile apps that engage users and drive business growth.',
     image: './icons/icons8-iphone-64.png',
     title: 'Mobile Development',
   },
 
   {
     description:
-      'We offer comprehensive services in building and managing cloud-based infrastructure using AWS. From architectural design to deployment and optimization, we leverage AWS technologies to ensure scalable, secure, and highly available cloud solutions that empower your business to thrive in the digital era.',
+      'From architectural design to deployment and optimization, we leverage AWS technologies to ensure scalable, secure, and highly available cloud solutions that empower your business to thrive in the digital era.',
+
+    image: './icons/icons8-cloud-done-64.png',
     title: 'Cloud-based Infrastructure',
   },
 
   {
     description:
-      'We have a strong focus on developing e-commerce websites using WooCommerce. From intuitive design and seamless integration to custom theme and plugin development, we provide tailored solutions that enhance user experiences, drive conversions, and empower businesses to succeed in the online marketplace.',
+      'From intuitive design and seamless integration to custom theme and plugin development, we provide tailored solutions that enhance user experiences, drive conversions, and empower businesses to succeed in the online marketplace.',
+
+    image: './icons/icons8-shopping-cart-64.png',
     title: 'E-commerce Development',
   },
 ]
