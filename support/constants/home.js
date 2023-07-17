6dc0c11c54189f93dc583047d910e92e7447196a
Federico Biondi <fdbion@gmail.com>
2023-07-17 13:24:29 -0300

    fix: hero texts (#261)

--- support/constants/home.js
+++ support/constants/home.js
@@ -1,37 +1,31 @@
 export const servicesCategories = [
   {
     description:
-      'We have rock-solid DevOps/SLA practices to help you leverage cloud technology speeding up development and increasing app security and scalability.',
-    image: './icons/icons8-cloud-done-64.png',
+      'We have rock-solid DevOps/SLA practices to help you leverage cloud technology speeding up development and increasing app security and scalability',
     title: 'Cloud Technology',
   },
   {
     description:
-      'Ship your MVP to market without spending months on assembling an in-house development team for your startup.',
-    image: './icons/icons8-process-64.png',
+      'Ship your MVP to market without spending months on assembling an in-house development team for your startup',
     title: 'MVP Development',
   },
   {
     description: 'Take your business prototype app for a test drive',
-    image: './icons/icons8-web-design-64.png',
     title: 'UX Design & Testing',
   },
   {
     description:
       'Create a beautiful and understandable interface to delight your users',
-    image: './icons/icons8-light-64.png',
     title: 'UI Design & Branding',
   },
   {
     description:
       'Well-designed and colorful apps, personalized on your brand’s concept and values, to build trust with your users',
-    image: './icons/icons8-tune-64.png',
     title: 'Custom Designed',
   },
   {
     description:
       'Responsive and accessible user interfaces that allow more people to access it from different types of devices with different needs',
-    image: './icons/icons8-amiable-64.png',
     title: 'User Friendly',
   },
 ]
@@ -42,7 +36,6 @@ export const servicesTypes = [
   {
     description:
       'From responsive design to backend development and deployment, we deliver high-quality web applications that enhance user experiences and drive your online presence forward.',
-
     image: './icons/icons8-source-code-64.png',
     title: 'Web Development',
   },
@@ -57,7 +50,6 @@ export const servicesTypes = [
   {
     description:
       'From architectural design to deployment and optimization, we leverage AWS technologies to ensure scalable, secure, and highly available cloud solutions that empower your business to thrive in the digital era.',
-
     image: './icons/icons8-cloud-done-64.png',
     title: 'Cloud-based Infrastructure',
   },
@@ -65,7 +57,6 @@ export const servicesTypes = [
   {
     description:
       'From intuitive design and seamless integration to custom theme and plugin development, we provide tailored solutions that enhance user experiences, drive conversions, and empower businesses to succeed in the online marketplace.',
-
     image: './icons/icons8-shopping-cart-64.png',
     title: 'E-commerce Development',
   },
