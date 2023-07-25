52bf002df72c447618fecacccbd1ad1f8914cd58
fbiondi <federico.biondi@rms.com>
2023-07-25 01:44:57 -0300

    deploy scripts update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/pollux-site/',
+
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
