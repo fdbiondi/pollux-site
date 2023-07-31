72f4a5c7265b39f4ead2386104a2102eadc0c20a
Federico Biondi <fdbion@gmail.com>
2023-07-31 14:11:06 -0300

    chore: privacy policy fisherton mall (#283)

---
+++ pages/fisherton-mall-privacidad.vue
@@ -0,0 +1,270 @@
+<template>
+  <section class="container m-auto py-24">
+    <p>Fisherton Mall Plaza recoge algunos datos personales de sus usuarios.</p>
+    <p>Tipos de datos recolectados</p>
+    <p>
+      Entre los tipos de datos personales que esta aplicación recopila, por sí
+      mismo o a través de terceros, se encuentran: posición geográfica, datos de
+      cookies y uso. Otros datos personales recopilados se pueden describir en
+      otras secciones de esta política de privacidad o mediante un texto de
+      explicación dedicado contextualmente con la recopilación de datos. Los
+      datos personales pueden ser proporcionados libremente por el usuario o
+      recopilados automáticamente cuando se utiliza esta aplicación. Cualquier
+      uso de Cookies -o de otras herramientas de seguimiento- por esta
+      Aplicación o por los propietarios de servicios de terceros utilizados por
+      esta Aplicación, a menos que se indique lo contrario, sirve para
+      identificar a los Usuarios y recordar sus preferencias, con el único
+      propósito de proporcionar el servicio requerido por el usuario. Si no se
+      proporcionan ciertos datos personales, es posible que esta aplicación no
+      pueda brindar sus servicios. El Usuario asume la responsabilidad de los
+      Datos Personales de terceros publicados o compartidos a través de esta
+      Aplicación y declara tener el derecho de comunicarlos o transmitirlos,
+      liberando así al Controlador de Datos de toda responsabilidad.
+    </p>
+    <p>Modo y lugar de procesamiento de los datos</p>
+    <p>Métodos de procesamiento</p>
+    <p>
+      El Controlador de Datos procesa los Datos de los Usuarios de manera
+      adecuada y tomará las medidas de seguridad adecuadas para evitar el acceso
+      no autorizado, la divulgación, la modificación o la destrucción no
+      autorizada de los Datos. El procesamiento de datos se lleva a cabo
+      utilizando computadoras y/o herramientas habilitadas, siguiendo los
+      procedimientos de la organización y los modos estrictamente relacionados
+      con los fines indicados. Además del controlador de datos, en algunos
+      casos, los datos pueden ser accesibles para ciertos tipos de personas a
+      cargo, involucradas con el funcionamiento del sitio (administración,
+      ventas, marketing, legal, administración de sistemas) o partes externas
+      (como terceros). proveedores de servicios técnicos de parte, carteros,
+      proveedores de hosting, compañías de TI, agencias de comunicación)
+      nombrados, si es necesario, como Procesadores de Datos por el Propietario.
+      La lista actualizada de estas partes puede solicitarse al Controlador de
+      datos en cualquier momento.
+    </p>
+    <p>Lugar</p>
+    <p>
+      Los datos se procesan en las oficinas de operación del controlador de
+      datos y en cualquier otro lugar donde se encuentren las partes
+      involucradas en el procesamiento. Para obtener más información, póngase en
+      contacto con el controlador de datos.
+    </p>
+    <p>Tiempo de retención</p>
+    <p>
+      Los Datos se conservan durante el tiempo necesario para proporcionar el
+      servicio solicitado por el Usuario, o establecidos por los fines descritos
+      en este documento, y el Usuario siempre puede solicitar que el Controlador
+      de Datos suspenda o elimine los datos.
+    </p>
+    <p>El uso de los datos recopilados</p>
+    <p>
+      Los datos relativos al usuario se recopilan para permitir que la
+      aplicación proporcione sus servicios, así como para los siguientes
+      propósitos: acceso a cuentas de servicios de terceros, interacciones
+      basadas en ubicación, comentarios de contenido e interacción con
+      plataformas y redes sociales externas. Los datos personales utilizados
+      para cada fin se describen en las secciones específicas de este documento.
+    </p>
+    <p>Permisos de Facebook solicitados por esta aplicación</p>
+    <p>
+      Esta Aplicación puede solicitar algunos permisos de Facebook que le
+      permitan realizar acciones con la cuenta de Facebook del Usuario y
+      recuperar de ella información, incluidos Datos Personales. Para obtener
+      más información sobre los siguientes permisos, consulte la documentación
+      de permisos de Facebook y la política de privacidad de Facebook. Los
+      permisos solicitados son los siguientes:
+    </p>
+    <p>Información básica</p>
+    <p>
+      De forma predeterminada, esto incluye ciertos datos del usuario, como id,
+      nombre, imagen, género y su configuración regional. Ciertas conexiones del
+      usuario, como Amigos, también están disponibles. Si el usuario ha hecho
+      más públicos sus datos, habrá más información disponible.
+    </p>
+    <p>Email</p>
+    <p>
+      Proporciona acceso a la dirección de correo electrónico principal del
+      usuario.
+    </p>
+    <p>Información detallada sobre el procesamiento de datos personales</p>
+    <p>
+      Los datos personales se recopilan para los siguientes propósitos y
+      utilizando los siguientes servicios:
+    </p>
+    <p>Acceso a cuentas de servicios de terceros</p>
+    <p>
+      Estos servicios le permiten a esta Aplicación acceder a los Datos de su
+      cuenta en un servicio de terceros y realizar acciones con ellos. Estos
+      servicios no se activan automáticamente, sino que requieren una
+      autorización explícita del usuario.
+    </p>
+    <p>Acceso a la cuenta de Facebook (esta aplicación)</p>
+    <p>
+      Este servicio permite que esta aplicación se conecte con la cuenta del
+      usuario en la red social Facebook, proporcionada por Facebook Inc.
+      Permisos solicitados: información básica y email. Lugar de procesamiento:
+      EE.UU. – Política de privacidad.
+    </p>
+    <p>Interacción con redes sociales externas y plataformas</p>
+    <p>
+      Estos servicios permiten la interacción con redes sociales u otras
+      plataformas externas directamente desde las páginas de esta Aplicación. La
+      interacción y la información obtenida por esta Aplicación siempre están
+      sujetas a la configuración de privacidad del Usuario para cada red social.
+      Si se instala un servicio que permite la interacción con redes sociales,
+      aún puede recopilar datos de tráfico para las páginas donde está instalado
+      el servicio, incluso cuando los usuarios no lo utilicen.
+    </p>
+    <p>Interacciones basadas en la ubicación</p>
+    <p>Geolocalización (esta aplicación)</p>
+    <p>
+      Esta aplicación puede recopilar, usar y compartir datos de ubicación del
+      usuario para proporcionar servicios basados en la ubicación. La mayoría de
+      los navegadores y dispositivos proporcionan herramientas para inhabilitar
+      esta función de manera predeterminada. Si se ha proporcionado una
+      autorización explícita, la aplicación puede rastrear los datos de
+      ubicación del usuario. Datos personales recopilados: posición geográfica.
+    </p>
+    <p>
+      Información adicional sobre la recopilación y el procesamiento de datos
+    </p>
+    <p>Acción legal</p>
+    <p>
+      Los Datos Personales del Usuario pueden ser utilizados con fines legales
+      por el Controlador de Datos, en el Tribunal o en las etapas que conducen a
+      posibles acciones legales derivadas del uso indebido de esta Aplicación o
+      los servicios relacionados.
+    </p>
+    <p>
+      El Usuario es consciente del hecho de que es posible que el Controlador de
+      Datos deba revelar datos personales a petición de las autoridades
+      públicas.
+    </p>
+    <p>Información adicional sobre los datos personales del usuario</p>
+    <p>
+      Además de la información contenida en esta política de privacidad, esta
+      Aplicación puede proporcionarle al Usuario información adicional y
+      contextual sobre servicios particulares o la recopilación y procesamiento
+      de Datos Personales a pedido.
+    </p>
+    <p>Registros del sistema y mantenimiento</p>
+    <p>
+      Para fines de operación y mantenimiento, esta Aplicación y los servicios
+      de terceros pueden recopilar archivos que registran la interacción con
+      esta Aplicación (Registros del sistema) o utilizar para este propósito
+      otros Datos personales (como la Dirección IP).
+    </p>
+    <p>Información no incluida en esta política</p>
+    <p>
+      Se pueden solicitar más detalles sobre la recopilación o el procesamiento
+      de Datos Personales desde el Controlador de Datos en cualquier momento.
+      Por favor, consulte la información de contacto al comienzo de este
+      documento.
+    </p>
+    <p>Los derechos de los usuarios</p>
+    <p>
+      Los usuarios tienen derecho, en cualquier momento, a saber si sus datos
+      personales han sido almacenados y pueden consultar el controlador de datos
+      para conocer sus contenidos y origen, verificar su exactitud o solicitar
+      que se completen, cancelen, actualicen o corrijan, o para su
+      transformación en formato anónimo o para bloquear cualquier información
+      que se tenga en violación de la ley, así como para oponerse a su
+      tratamiento por cualquier razón legítima. Las solicitudes deben enviarse
+      al Controlador de datos a la información de contacto establecida
+      anteriormente.
+    </p>
+    <p>Esta aplicación no admite solicitudes de «No rastrear».</p>
+    <p>
+      Para determinar si alguno de los servicios de terceros que utiliza cumple
+      con las solicitudes de «No rastrear», lea sus políticas de privacidad.
+    </p>
+    <p>Cambios a esta política de privacidad</p>
+    <p>
+      El Controlador de Datos se reserva el derecho de realizar cambios a esta
+      política de privacidad en cualquier momento mediante notificación a sus
+      Usuarios en esta página. Se recomienda encarecidamente que consulte esta
+      página a menudo, en referencia a la fecha de la última modificación que
+      figura en la parte inferior. Si un Usuario se opone a cualquiera de los
+      cambios a la Política, el Usuario debe dejar de usar esta Aplicación y
+      puede solicitar que el Controlador de Datos elimine los Datos Personales.
+      A menos que se indique lo contrario, la política de privacidad vigente en
+      ese momento se aplica a todos los Datos Personales que el Controlador de
+      Datos tenga sobre los Usuarios.
+    </p>
+    <p>Definiciones y referencias legales</p>
+    <p>Datos personales (o datos)</p>
+    <p>
+      Cualquier información sobre una persona física, una persona jurídica, una
+      institución o una asociación, que se identifica o se puede identificar,
+      incluso indirectamente, mediante referencia a cualquier otra información,
+      incluido un número de identificación personal.
+    </p>
+    <p>Datos de uso</p>
+    <p>
+      Información recopilada automáticamente de esta Aplicación (o servicios de
+      terceros empleados en esta Aplicación), que puede incluir: las direcciones
+      IP o nombres de dominio de las computadoras utilizadas por los Usuarios
+      que usan esta Aplicación, las direcciones URI (Identificador Uniforme de
+      Recursos), el tiempo de la solicitud, el método utilizado para enviar la
+      solicitud al servidor, el tamaño del archivo recibido en respuesta, el
+      código numérico que indica el estado de la respuesta del servidor
+      (resultado exitoso, error, etc.), el país de origen, el características
+      del navegador y del sistema operativo utilizado por el Usuario, los
+      diversos detalles de tiempo por visita (por ejemplo, el tiempo dedicado a
+      cada página dentro de la Aplicación) y los detalles sobre la ruta seguida
+      dentro de la Aplicación con referencia especial a la secuencia de páginas
+      visitado, y otros parámetros sobre el sistema operativo del dispositivo
+      y/o el entorno de TI del usuario.
+    </p>
+    <p>Usuario</p>
+    <p>
+      El individuo que usa esta Aplicación, que debe coincidir o ser autorizado
+      por el Sujeto de Datos, a quien se refieren los Datos Personales.
+    </p>
+    <p>Sujeto de datos</p>
+    <p>
+      La persona física o jurídica a quien se refieren los datos personales.
+    </p>
+    <p>Procesador de Datos (o Supervisor de Datos)</p>
+    <p>
+      La persona física, persona jurídica, administración pública o cualquier
+      otro organismo, asociación u organización autorizada por el Controlador de
+      Datos para procesar los Datos Personales de conformidad con esta política
+      de privacidad.
+    </p>
+    <p>Controlador de datos (o propietario)</p>
+    <p>
+      La persona física, persona jurídica, administración pública o cualquier
+      otro organismo, asociación u organización con derecho, también
+      conjuntamente con otro Controlador de Datos, para tomar decisiones con
+      respecto a los propósitos, y los métodos de procesamiento de los Datos
+      Personales y los medios utilizados, incluido el medidas de seguridad
+      concernientes al funcionamiento y uso de esta Aplicación. El controlador
+      de datos, a menos que se especifique lo contrario, es el propietario de
+      esta aplicación.
+    </p>
+    <p>Esta aplicación</p>
+    <p>
+      La herramienta de hardware o software por la que se recopilan los datos
+      personales del usuario.
+    </p>
+    <p>Cookies</p>
+    <p>
+      Una pequeña porción de información almacenada en el dispositivo del
+      usuario.
+    </p>
+    <p>Información legal</p>
+    <p>
+      Aviso para los usuarios europeos: esta declaración de privacidad se ha
+      preparado en cumplimiento de las obligaciones establecidas en el art. 10
+      de la Directiva de la CE n. 95/46 / CE, y en virtud de las disposiciones
+      de la Directiva 2002/58 / CE, revisada por la Directiva 2009/136 / CE,
+      sobre el tema de Cookies. Esta política de privacidad se relaciona
+      únicamente con esta Aplicación.
+    </p>
+  </section>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'empty',
+});
+</script>
