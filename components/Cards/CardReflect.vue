fa4c4908d546db7e04cf71133fe6e0d568511bee
fbiondi <fdbion@gmail.com>
2024-04-24 22:14:01 -0300

    remove

--- components/Cards/CardReflect.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="card h-full cursor-pointer border-4 border-gray-800 px-8 py-8 hover:bg-gray-800 hover:text-white hover:shadow-xl hover:shadow-gray-400 dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black-light dark:hover:shadow-gray-200"
-  >
-    <slot />
-  </div>
-</template>
-
-<script setup></script>
